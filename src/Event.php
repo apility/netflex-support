df2525576c5cabaa53b7cd3d7a3f56100fe661ed
Thomas Alrek <thomas@alrek.no>
2019-12-17 16:19:29 +0100

    Add missing typings

--- src/Event.php
+++ src/Event.php
@@ -10,24 +10,40 @@ class Event
   /** @var callable */
   public $handler;
 
+  /**
+   * @param string $name = null
+   * @param callable $handler = null
+   */
   public function __construct($name = null, $handler = null)
   {
     $this->name = $name;
     $this->handler = $handler;
   }
 
+  /**
+   * @param string $name
+   * @return static
+   */
   public function setName($name)
   {
     $this->name = $name;
     return $this;
   }
 
+  /**
+   * @param callable $handler
+   * @return static
+   */
   public function setHandler($handler)
   {
     $this->handler = $handler;
     return $this;
   }
 
+  /**
+   * @param mixed $payload
+   * @return mixed
+   */
   public function handle($payload = null)
   {
     if (is_callable($this->handler)) {
