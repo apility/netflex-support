3180fbd6e6ee7ddeee8ccece330a7b61b6ac9f53
Thomas Alrek <thomas@alrek.no>
2019-12-17 10:12:28 +0100

    Implement Event abstraction

---
+++ src/Event.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Netflex\Support;
+
+class Event {
+  /** @var string */
+  public $name;
+
+  /** @var callable */
+  public $handler;
+
+  public function __construct ($name = null, $handler = null) {
+    $this->name = $name;
+    $this->handler = $handler;
+  }
+
+  public function setName ($name) {
+    $this->name = $name;
+    return $this;
+  }
+
+  public function setHandler ($handler) {
+    $this->handler = $handler;
+    return $this;
+  }
+
+  public function handle ($payload = null) {
+    if (is_callable($this->handler)) {
+      return ($this->handler)($payload);
+    }
+  }
+}
