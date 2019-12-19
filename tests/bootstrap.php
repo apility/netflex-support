3d5d4aeadd8efaf610e64737f145ae9d59782382
Thomas Alrek <thomas@alrek.no>
2019-12-19 12:36:17 +0100

    Update test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,5 @@
 require_once('Mocks/TestObject.php');
 require_once('Mocks/TestCollection.php');
 require_once('Mocks/TestObjectWithDefaults.php');
+require_once('Mocks/TestObjectWithTimestamps.php');
+require_once('Mocks/TestTrait.php');
